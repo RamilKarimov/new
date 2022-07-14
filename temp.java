b90960a0caf7e28d55e5fa5d037718eb2627fe91
Ramil <109086878+RamilKarimov@users.noreply.github.com>
2022-07-14 08:54:34 +0400

    Add files via upload

---
+++ temp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class temp {
+    public class main {
+        public static void main(String[] args) {
+            String cold = "на улице холодно";
+            String warm = "на улице тепло";
+            Scanner console = new Scanner(System.in);
+            int temp = console.nextInt();
+            if (temp < 0)
+                System.out.println("на улице холодно");
+            else
+                System.out.println("на улице тепло");
+        }
+    }
+
+}
